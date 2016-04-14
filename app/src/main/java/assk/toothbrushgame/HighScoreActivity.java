c45cabc39d97a991ff440ab28ca8e5e8631550b6
Tomas Kacer <Tomas.Kacer@anritsu.com>
2016-04-14 14:46:02 +0200

    initial commit

---
+++ app/src/main/java/assk/toothbrushgame/HighScoreActivity.java
@@ -0,0 +1,9 @@
+package assk.toothbrushgame;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tomas Kacer on 14. 4. 2016.
+ */
+public class HighScoreActivity extends AppCompatActivity {
+}
