117f526925c8250295590ac6209c4244f542e520
Tomas Kacer <Tomas.Kacer@anritsu.com>
2016-04-14 19:58:00 +0200

    accelerometer + emotion face

--- app/src/main/java/assk/toothbrushgame/GameActivity.java
+++ app/src/main/java/assk/toothbrushgame/GameActivity.java
@@ -1,9 +1,131 @@
 package assk.toothbrushgame;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Tomas Kacer on 14. 4. 2016.
  */
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements SensorEventListener {
+
+    private ImageView dirtLT;
+    private ImageView dirtCT;
+    private ImageView dirtRT;
+    private ImageView dirtLB;
+    private ImageView dirtCB;
+
+    private ImageView foamLT;
+    private ImageView foamCT;
+    private ImageView foamRT;
+    private ImageView foamLB;
+    private ImageView foamCB;
+
+    private ImageView emotion;
+
+    private View object;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    private long lastUpdate;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+
+        object = findViewById(R.id.object);
+//        object.setX
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    /**
+     * Called when sensor values have changed.
+     * <p>See {@link SensorManager SensorManager}
+     * for details on possible sensor types.
+     * <p>See also {@link SensorEvent SensorEvent}.
+     * <p/>
+     * <p><b>NOTE:</b> The application doesn't own the
+     * {@link SensorEvent event}
+     * object passed as a parameter and therefore cannot hold on to it.
+     * The object may be part of an internal pool and may be reused by
+     * the framework.
+     *
+     * @param event the {@link SensorEvent SensorEvent}.
+     */
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = event.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+//                float speed = Math.abs(x + y + z - last_x - last_y - last_z)/diffTime * 10000;
+                float speed = Math.abs(y - last_y) / diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+
+                }
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Called when the accuracy of the registered sensor has changed.
+     * <p/>
+     * <p>See the SENSOR_STATUS_* constants in
+     * {@link SensorManager SensorManager} for details.
+     *
+     * @param sensor
+     * @param accuracy The new accuracy of this sensor, one of
+     *                 {@code SensorManager.SENSOR_STATUS_*}
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
--- app/src/main/java/assk/toothbrushgame/VoCoGoActivity.java
+++ app/src/main/java/assk/toothbrushgame/VoCoGoActivity.java
@@ -6,4 +6,5 @@ import android.support.v7.app.AppCompatActivity;
  * Created by Tomas Kacer on 14. 4. 2016.
  */
 public class VoCoGoActivity extends AppCompatActivity {
+
 }
